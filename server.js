bb5b2895e4b45e61f0eac7bc88ea77fe0a762abe
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2024-08-14 09:43:08 +0600

    update cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 // middleware
 app.use(cors({
-    origin: ['http://localhost:5173']
+    origin: ['http://localhost:5173', 'https://toolskit-org.web.app']
 }))
 
 app.use(express.json());
